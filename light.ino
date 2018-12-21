b66e436f0f548998dc86db174c1854569a74cd25
Madeline Carr <Carrm2@vcu.edu>
2018-12-20 23:02:16 -0500

    Started the Tune
    I added a blinking pattern to the green led that goes to the tune of Jingle Bells.

--- light.ino
+++ light.ino
@@ -1,16 +1,64 @@
 
 int greenPin = 8;
-int delayPeriod = 500;
+
+int veryshortPeriod = 100;
+int shortPeriod = 300;
+int longPeriod = 600;
 
 void setup()
 {
-    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+    pinMode(greenPin, OUTPUT);
 }
 
 void loop()
 {
+    // Jingle Bells
     digitalWrite(greenPin, HIGH);
-    delay(500);
+    delay(shortPeriod);
     digitalWrite(greenPin, LOW);
-    delay(500);
+    delay(shortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(shortPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(shortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(longPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(shortPeriod);
+    // Jingle Bells
+    digitalWrite(greenPin, HIGH);
+    delay(shortPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(shortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(shortPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(shortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(longPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(shortPeriod);
+    //Jingle all the Way
+    digitalWrite(greenPin, HIGH);
+    delay(shortPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(shortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(shortPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(shortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(shortPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(veryshortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(veryshortPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(veryshortPeriod);
+    digitalWrite(greenPin, HIGH);
+    delay(longPeriod);
+    digitalWrite(greenPin, LOW);
+    delay(longPeriod);
+    delay(6000);
 }
